ccb3c6852c624032967e10131452aa8652942e2c
Davichobits <davidruizdeveloper@gmail.com>
2023-08-03 07:51:21 -0500

    desktop view

---
+++ main.js
@@ -0,0 +1,32 @@
+const featuresBtn = document.querySelector('#features-btn')
+const featuresUl = document.querySelector('#features-ul')
+const featuresBtnArrow = document.querySelector('#features-btn-arrow')
+
+featuresBtn.addEventListener('click', ()=>{
+  featuresUl.style.display = (featuresUl.style.display === 'block') ? 'none' : 'block';
+  featuresBtnArrow.style.transition = 'transform 0.3s ease'
+  featuresBtnArrow.style.transform = (featuresBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)';
+})
+
+const companyBtn = document.querySelector('#company-btn')
+const companyUl = document.querySelector('#company-ul')
+const companyBtnArrow = document.querySelector('#company-btn-arrow')
+companyBtn.addEventListener('click', ()=>{
+  companyUl.style.display = (companyUl.style.display === 'block') ? 'none' : 'block';
+  companyBtnArrow.style.transition = 'transform 0.3s ease'
+  companyBtnArrow.style.transform = (companyBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)';
+})
+
+const closeBtn = document.querySelector('#close-btn')
+const navBar = document.querySelector('nav')
+const hamburgerNenu = document.querySelector('#hamburger-menu')
+
+closeBtn.addEventListener('click', ()=>{
+  navBar.style.display = 'none';
+  hamburgerNenu.style.display = 'block'
+})
+
+hamburgerNenu.addEventListener('click', ()=>{
+  navBar.style.display = 'block';
+  hamburgerNenu.style.display = 'none'
+})
